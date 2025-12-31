f0badd182a123ea7f17094d732e73becbdd7f0f5
hittalohenrick <henrickhittalo@gmail.com>
2025-12-31 19:55:26 -0400

    fix: increase pageLoadTimeout to 100s for CI stability

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
   e2e: {
+    pageLoadTimeout: 100000,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
